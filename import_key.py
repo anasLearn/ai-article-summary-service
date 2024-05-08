9eaf345017cffcb98a76d1a20483729bc166a3ac
anasa <anas.aamoum@gmail.com>
2024-05-08 19:35:34 +0300

    improved prompt

--- import_key.py
+++ import_key.py
@@ -11,5 +11,5 @@ load_dotenv()
 # defaults to getting the key using os.environ.get("OPENAI_API_KEY")
 # if you saved the key under a different environment variable name, you can do something like:
 client = OpenAI(
-  api_key=os.environ.get("OPENAI_API_KEY"),
+    api_key=os.environ.get("OPENAI_API_KEY"),
 )
--- prompt.py
+++ prompt.py
@@ -8,6 +8,15 @@ def generate_prompt(article: str):
     I will give you a news article. I want you to read it, and to summarize it in exactly 3 sentences. You will give me the sentences as bullet points.
     I want you to do the summary in 3 languages: Arabic, French, and English.
     
+    give me the result as a json variable looking like this;
+    {
+      "EN": ["first sentence", "second sentence", "thrid sentence"],
+      "FR": ["first sentence", "second sentence", "thrid sentence"],
+      "AR": ["first sentence", "second sentence", "thrid sentence"]
+    }
+    
+    Give me only the JSON object, nothing more, nothing less.
+    
     Here is the article:
     {article}
     """
