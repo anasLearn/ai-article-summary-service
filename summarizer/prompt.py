bc42b8f0b3132d34a555a756d7d17ca81465b352
Anas Aamoum <anas.aamoum@gmail.com>
2024-08-14 15:11:18 +0300

    Updated prompt

--- summarizer/prompt.py
+++ summarizer/prompt.py
@@ -1,32 +1,33 @@
 system_message = """
 You are a journalist student who speaks multiple languages. 
 You will help me by summarizing the articles I give you in several languages.
+
+Each time I give you a news article. I want you to read it, and to summarize it in a title and exactly 2 sentences. 
+You will give me the title and sentences as bullet points.
+I want you to do the summaries in 3 languages: Arabic, Finnish and English.
+
+When you want to make the summary in other languages (different from the language of the original article), 
+don't just translate the summary from the original language. But do get the summary in the target language by 
+reanalysing the original article. 
+
+For each article I give you, give me the result as a json variable looking like this:
+{{
+  "EN": ["title", "first sentence", "second sentence"],
+  "FR": ["title", "first sentence", "second sentence"],
+  "AR": ["title", "first sentence", "second sentence"]
+}}
+
+Make the sentences simple.
+Give me only the JSON object, nothing more, nothing less.
 """
 
 
 def generate_prompt(article: str):
     prompt = f"""
-    I will give you a news article. I want you to read it, and to summarize it in at most 3 sentences. 
-    You will give me the sentences as bullet points.
-    I want you to do the summary in 3 languages: Arabic, Finnish and English.
-    
-    When you want to make the summary in other languages (different from the language of the original article), 
-    don't just translate the summary from the original language. But do get the summary in the target language by 
-    reanalysing the original article. 
-    
-    give me the result as a json variable looking like this:
-    {{
-      "EN": ["first sentence", "second sentence", "third sentence"],
-      "FR": ["first sentence", "second sentence", "third sentence"],
-      "AR": ["first sentence", "second sentence", "third sentence"]
-    }}
-    
-    Make the sentences simple.
-    
-    Give me only the JSON object, nothing more, nothing less.
-    
     Here is the article:
     {article}
+    
+    Give me only the JSON object, nothing more, nothing less.
     """
 
     return prompt
