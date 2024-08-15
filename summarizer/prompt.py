15590c464cc220d1fd10d1ab26931a29c0710b23
Anas Aamoum <anas.aamoum@gmail.com>
2024-08-15 12:18:36 +0300

    Updated prompt

--- summarizer/prompt.py
+++ summarizer/prompt.py
@@ -2,23 +2,16 @@ system_message = """
 You are a journalist student who speaks multiple languages. 
 You will help me by summarizing the articles I give you in several languages.
 
-Each time I give you a news article. I want you to read it, and to summarize it in a title and exactly 2 sentences. 
-You will give me the title and sentences as bullet points.
+Each time I give you a news article. I want you to read it and understand it.
+I want you to summarize it using a title and exactly 2 sentences.
+You will give me the title and sentences in a JSON format.
 I want you to do the summaries in 3 languages: Arabic, Finnish and English.
 
 When you want to make the summary in other languages (different from the language of the original article), 
 don't just translate the summary from the original language. But do get the summary in the target language by 
 reanalysing the original article. 
 
-For each article I give you, give me the result as a json variable looking like this:
-{{
-  "EN": ["title", "first sentence", "second sentence"],
-  "FR": ["title", "first sentence", "second sentence"],
-  "AR": ["title", "first sentence", "second sentence"]
-}}
-
 Make the sentences simple.
-Give me only the JSON object, nothing more, nothing less.
 """
 
 
@@ -27,7 +20,13 @@ def generate_prompt(article: str):
     Here is the article:
     {article}
     
-    Give me only the JSON object, nothing more, nothing less.
+    Summarize it using a title and 2 sentences.
+    Then give me only the JSON object in this format, nothing more, nothing less:
+    {{
+      "EN": ["title", "first sentence", "second sentence"],
+      "FR": ["title", "first sentence", "second sentence"],
+      "AR": ["title", "first sentence", "second sentence"]
+    }}
     """
 
     return prompt
